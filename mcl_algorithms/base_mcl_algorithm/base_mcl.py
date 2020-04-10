fdcac5a881483f8967d801639bab0ebaf60920b6
StevenMHernandez <me@stevenmhernandez.com>
2020-04-10 13:02:19 -0400

    allow variable k_hop neighbors communication

--- mcl_algorithms/base_mcl_algorithm/base_mcl.py
+++ mcl_algorithms/base_mcl_algorithm/base_mcl.py
@@ -32,26 +32,41 @@ class BaseMCL(object):
             self.add_packet_communication('share_state_change_second_hop')
 
     def communication_share_trinary_connectivity_change_to_all_neighbors(self, nodes, previous_global_state_matrix, current_global_state_matrix, k_hop_neighbors):
-        one_hop_neighbors_which_received_update = []
-        for i, n1 in enumerate(nodes):
-            state_changed_between_any_neighbor = False
+        delta_messages_to_share = {}
+        delta_messages_shared = {}
 
+        for i, n1 in enumerate(nodes):
+            delta_messages_to_share[n1] = []
+            delta_messages_shared[n1] = []
+
+        for i, n1 in enumerate(nodes):
+            #
+            # Check if the the state changed between any of the neighbors
+            #
             for j, n2 in enumerate(nodes):
                 if previous_global_state_matrix is None or \
                         previous_global_state_matrix[i, j] != current_global_state_matrix[i, j]:
-                    state_changed_between_any_neighbor = True
+                    delta_messages_to_share[n2].append(n1)
 
-            if state_changed_between_any_neighbor:
-                for n2 in n1.one_hop_neighbors:
-                    self.add_packet_communication('share_state_change_first_hop')
-                    one_hop_neighbors_which_received_update.append(n2)
-        second_hop_neighbors_which_received_update = []
-        if k_hop_neighbors > 1:
-            for n2 in set(one_hop_neighbors_which_received_update):
-                self.add_packet_communication('share_state_change_second_hop')
-                if k_hop_neighbors > 2:
-                    for n3 in n2.one_hop_neighbors:
-                        self.add_packet_communication('share_state_change_third_hop')
+        for k in range(1,k_hop_neighbors+1):
+            new_delta_messages_to_share = {}
+            for i, n1 in enumerate(nodes):
+                new_delta_messages_to_share[n1] = []
+            for n1 in delta_messages_to_share.keys():
+                #
+                #  Find any non-duplicate messages
+                #
+                non_duplicates = []
+                for n2 in delta_messages_to_share[n1]:
+                    if n2 not in delta_messages_shared[n1]:
+                        non_duplicates.append(n2)
+
+                if len(non_duplicates) > 0:
+                    for n2 in n1.one_hop_neighbors:
+                        delta_messages_shared[n1] += non_duplicates
+                        new_delta_messages_to_share[n2] += non_duplicates
+                        self.add_packet_communication('share_state_change_' + str(k) + '_hop')
+            delta_messages_to_share = new_delta_messages_to_share
 
     def communication_share_gps_to_all_1_and_2_hop_neighbors(self, nodes, previous_global_state_matrix, current_global_state_matrix):
         for n1 in [n for n in nodes if n.is_anchor]:
